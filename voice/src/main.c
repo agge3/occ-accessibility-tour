e2fbed872ccd845b9eb1111722b5bca41c7df7b7
kpowkitty <witchikittikat@gmail.com>
2024-04-05 21:52:58 -0700

    adding deepspeech lib

---
+++ voice/src/main.c
@@ -0,0 +1,17 @@
+/* Project: occ-accessibility-tour
+ * Sub-project: voice
+ * Developers: @agge3, @kpowkitty
+ * Date: February 2024
+ */
+
+#include "deepspeech.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // init deepspeech
+    ModelState* ctx;
+
+    return 0;
+}
